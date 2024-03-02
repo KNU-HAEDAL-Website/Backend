f85d75e60cf254b42dfdec056636846363fc3784
tfer2442 <tfer2442@gmail.com>
2024-03-02 17:47:47 +0900

    chore(all): 스프링 프로젝트 생성 (#1)

---
+++ src/main/java/com/haedal/haedalweb/HaedalWebApplication.java
@@ -0,0 +1,13 @@
+package com.haedal.haedalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaedalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaedalWebApplication.class, args);
+	}
+
+}
