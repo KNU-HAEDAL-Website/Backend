c3854496c0eee7dc0c241e00e5c9993a072761cd
tfer2442 <tfer2442@gmail.com>
2024-03-05 17:33:32 +0900

    fix(HaedalWebApplication): 데이터베이스 자동 연결 설정 해제
    exclude옵션 추가
    
    resolves: #2

--- src/main/java/com/haedal/haedalweb/HaedalWebApplication.java
+++ src/main/java/com/haedal/haedalweb/HaedalWebApplication.java
@@ -2,12 +2,11 @@ package com.haedal.haedalweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class HaedalWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HaedalWebApplication.class, args);
 	}
-
 }
