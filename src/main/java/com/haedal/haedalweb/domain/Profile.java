5ca3f90b2545bf4214233ccfd4a77e836fbbcfbb
tfer2442 <tfer2442@gmail.com>
2024-03-10 16:47:36 +0900

    feat(User, Profile, Sns): User, Profile, Sns Entity 생성
    User, Profile, Sns Entity를 생성
    User와 Profile 그리고 Profile과 Sns 사이의 연관관계를 설정
    
    related to: #5

---
+++ src/main/java/com/haedal/haedalweb/domain/Profile.java
@@ -0,0 +1,28 @@
+package com.haedal.haedalweb.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Profile {
+    @Id
+    @Column(name = "profile_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "profile_intro", length = 127)
+    private String intro;
+
+    @Column(name = "profile_image_url")
+    private String imageUrl;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "sns_id")
+    private Sns sns;
+}
