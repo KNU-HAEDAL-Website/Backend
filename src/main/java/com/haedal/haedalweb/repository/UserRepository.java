448320fef60eae3ff8a9d318b6415f0382b42ca3
tfer2442 <tfer2442@gmail.com>
2024-03-30 15:25:03 +0900

    feat(UserRepository): DB에 입력 받은 id가 존재하는지 확인하는 메서드 생성 (#6)

--- src/main/java/com/haedal/haedalweb/repository/UserRepository.java
+++ src/main/java/com/haedal/haedalweb/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Boolean existsByStudentNumber(Long id);
+    boolean existsByStudentNumber(Integer studentNumber);
+    boolean existsById(String id);
 }
