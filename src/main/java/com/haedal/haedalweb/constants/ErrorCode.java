9419f476ea353da5c2369668a43761b75b0c0c5a
tfer2442 <tfer2442@gmail.com>
2024-04-18 03:29:13 +0900

    feat: 요청 성공 반환 관련 클래스 및 userId, studentNumber 중복 검사 로직 작성 (#17)

--- src/main/java/com/haedal/haedalweb/constants/ErrorCode.java
+++ src/main/java/com/haedal/haedalweb/constants/ErrorCode.java
@@ -10,7 +10,6 @@ public enum ErrorCode {
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, "중복된 아이디가 존재합니다."),
     DUPLICATED_STUDENT_NUMBER(HttpStatus.CONFLICT, "중복된 학번이 존재합니다.");
 
-
     private final HttpStatus httpStatus;
     private final String message;
 }
--- src/main/java/com/haedal/haedalweb/controller/JoinController.java
+++ src/main/java/com/haedal/haedalweb/controller/JoinController.java
@@ -1,15 +1,20 @@
 package com.haedal.haedalweb.controller;
 
+import com.haedal.haedalweb.constants.SuccessCode;
 import com.haedal.haedalweb.dto.JoinDTO;
+import com.haedal.haedalweb.dto.SuccessResponse;
 import jakarta.validation.Valid;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.haedal.haedalweb.service.JoinService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
+@RequestMapping("/join")
 public class JoinController {
     private final JoinService joinService;
 
@@ -17,10 +22,46 @@ public class JoinController {
         this.joinService = joinService;
     }
 
-    @PostMapping("/join")
-    public String joinProcess(@RequestBody @Valid JoinDTO joinDTO){
+    @PostMapping
+    public ResponseEntity<SuccessResponse> resisterUser(@RequestBody @Valid JoinDTO joinDTO) {
+        joinService.createUserAccount(joinDTO);
 
-        joinService.joinProcess(joinDTO);
-        return "ok";
+        SuccessCode successCode = SuccessCode.JOIN_SUCCESS;
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .success(successCode.getSuccess())
+                .message(successCode.getMessage())
+                .build();
+
+        return ResponseEntity.status(successCode.getHttpStatus()).body(successResponse);
+    }
+
+    @GetMapping("/check-user-id")
+    public ResponseEntity<SuccessResponse> checkUserIdDuplicate(@RequestParam String userId) {
+        SuccessCode successCode = SuccessCode.UNIQUE_USER_ID;
+        if (joinService.isUserIdDuplicate(userId)) {
+            successCode = SuccessCode.DUPLICATED_USER_ID;
+        }
+
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .success(successCode.getSuccess())
+                .message(successCode.getMessage())
+                .build();
+
+        return ResponseEntity.status(successCode.getHttpStatus()).body(successResponse);
+    }
+
+    @GetMapping("/check-student-number")
+    public ResponseEntity<SuccessResponse> checkStudentNumberDuplicate(@RequestParam Integer studentNumber) {
+        SuccessCode successCode = SuccessCode.UNIQUE_STUDENT_NUMBER;
+        if (joinService.isStudentNumberDuplicate(studentNumber)) {
+            successCode = SuccessCode.DUPLICATED_STUDENT_NUMBER;
+        }
+
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .success(successCode.getSuccess())
+                .message(successCode.getMessage())
+                .build();
+
+        return ResponseEntity.status(successCode.getHttpStatus()).body(successResponse);
     }
 }
