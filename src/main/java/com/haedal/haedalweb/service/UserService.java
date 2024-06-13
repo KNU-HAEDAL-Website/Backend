668c43bebce45e4dd8d2b220fa28c7b8ee9e5c03
tfer2442 <tfer2442@gmail.com>
2024-06-13 16:38:47 +0900

    feat: 현재 로그인한 사용자 반환 메서드 작성

--- src/main/java/com/haedal/haedalweb/service/UserService.java
+++ src/main/java/com/haedal/haedalweb/service/UserService.java
@@ -2,11 +2,13 @@ package com.haedal.haedalweb.service;
 
 import com.haedal.haedalweb.constants.ErrorCode;
 import com.haedal.haedalweb.domain.User;
+import com.haedal.haedalweb.dto.CustomUserDetails;
 import com.haedal.haedalweb.exception.BusinessException;
 import com.haedal.haedalweb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -21,5 +23,13 @@ public class UserService {
 
     public List<User> findUserByIds(List<String> userIds) {
         return userRepository.findAllById(userIds);
+    }
+
+    public User getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String userId = userDetails.getUsername();
+
+        return findUserById(userId);
     }
 }
