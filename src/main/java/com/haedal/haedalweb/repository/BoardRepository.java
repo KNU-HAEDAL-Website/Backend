052991021f785c5e0262460c7467bc97baf37ecc
tfer2442 <tfer2442@gmail.com>
2024-06-18 16:02:37 +0900

    feat: Board, Participant, User Fetch Join 쿼리 작성 (#54)

--- src/main/java/com/haedal/haedalweb/repository/BoardRepository.java
+++ src/main/java/com/haedal/haedalweb/repository/BoardRepository.java
@@ -1,10 +1,16 @@
 package com.haedal.haedalweb.repository;
 
 import com.haedal.haedalweb.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
     boolean existsByActivityId(Long activityId);
+
+    @Query("select distinct b from Board b join fetch b.participants p join fetch p.user where b.activity.id = :activityId")
+    Page<Board> findBoardsByActivityId(Long activityId, Pageable pageable);
 }
