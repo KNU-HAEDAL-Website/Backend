2a98d09d27424129eb7ee30a168af863925d1017
tfer2442 <tfer2442@gmail.com>
2024-08-27 20:11:51 +0900

    chore: CORS 허용

--- src/main/java/com/haedal/haedalweb/config/CorsMvcConfig.java
+++ src/main/java/com/haedal/haedalweb/config/CorsMvcConfig.java
@@ -10,6 +10,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000", "http://www.knu-haedal.com");
     }
 }
