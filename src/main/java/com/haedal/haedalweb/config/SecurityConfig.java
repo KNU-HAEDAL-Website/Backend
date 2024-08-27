0c2a1b7791221556497643c0ea3d0ea74c5bfd57
tfer2442 <tfer2442@gmail.com>
2024-08-27 20:26:19 +0900

    feat: cors 필터 오픈 (#85)

--- src/main/java/com/haedal/haedalweb/config/SecurityConfig.java
+++ src/main/java/com/haedal/haedalweb/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000, http://www.knu-haedal.com"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
